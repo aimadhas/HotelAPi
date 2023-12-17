ac3ee6a19b969a110feb4032164a5f499ffe748c
aimad-hasnaoui <aimadha1234@gmail.com>
2023-12-17 13:07:24 +0100

    add new controller

---
+++ UdemyTestAplication/Data/Hotel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UdemyTestAplication.Data
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Adrees { get; set; }
+
+        public string  Descreption { get; set; }
+        public double Rating { get; set; }
+        [ForeignKey(nameof(CountryId))]
+        public int CountryId { get; set; }
+
+        public Country country { get; set; }
+    }
+}
---
+++ UdemyTestAplication/Data/HotelDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UdemyTestAplication.Data
+{
+    public class HotelDbContext : DbContext
+    {
+        public HotelDbContext(DbContextOptions options) : base(options) { 
+        
+       
+        }
+        public DbSet<Hotel> Hotel { get; set; }
+        public DbSet<Country> Countries { get; set; }
+    }
+}
